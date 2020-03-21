107165e828447ab35265a3536f00b6cff1f7c895
yami <yaminet@sina.com>
2020-03-21 21:51:24 +0800

    更新

--- lib/grpclib.dart
+++ lib/grpclib.dart
@@ -1,4 +1,8 @@
 library grpclib;
+export 'src/generated/helloworld.pbgrpc.dart';
+export 'src/generated/helloworld.pb.dart';
+export 'src/generated/helloworld.pbenum.dart';
+export 'src/generated/helloworld.pbjson.dart';
 
 /// A Calculator.
 class Calculator {
